8783247ad8f0ff22a3df1680f132d19c889f50fe
박정현 <pjh9996@naver.com>
2020-11-16 04:13:05 +0900

    Create titanic_02.py

---
+++ src/titanic_02.py
@@ -0,0 +1,20 @@
+# https://www.kaggle.com/c/titanic/data
+# 데이터 컬럼 분석
+
+import pandas as pd
+
+data = pd.read_csv('../data/train.csv')
+print(data.columns)
+
+# PassengerId: 승객 고유 ID
+# Survived: 생존 여부 (0: 사망, 0: 생존)
+# Pclass: 좌석 등급 (높을수록 좋은 좌석)
+# Name: 승객 이름
+# Sex: 승객 성별
+# Age: 승객 나이(1보다 작은 경우 분수, 추정치일 경우 .5)
+# SibSp: 동승 형제/자매/배우자 수
+# Parch: 동승 부모/자식 수
+# Ticket: 티켓 번호
+# Fare: 요금
+# Cabin: 객실 번호
+# Embarked: 정박 항구(C = Cherbourg, Q = Queenstown, S = Southampton)
