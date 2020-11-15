d953163dc3a4085ce478e2ff708cab67bfcb44ba
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2020-11-16 08:34:23 +0900

    Update 15-pandas-fill-value.py

--- src/15-pandas-fill-value.py
+++ src/15-pandas-fill-value.py
@@ -1,6 +1,3 @@
-# https://www.kaggle.com/c/titanic/data
-# 결측값(null) 채우기
-
 import pandas as pd
 
 data = pd.read_csv('../data/train.csv')
@@ -10,19 +7,3 @@ data['Cabin'] = data['Cabin'].fillna('N')
 data['Embarked'] = data['Embarked'].fillna('S')
 
 print(data.isnull().sum())
-
-# PassengerId    0
-# Survived       0
-# Pclass         0
-# Name           0
-# Sex            0
-# Age            0
-# SibSp          0
-# Parch          0
-# Ticket         0
-# Fare           0
-# Cabin          0
-# Embarked       0
-# dtype: int64
-#
-# Process finished with exit code 0
