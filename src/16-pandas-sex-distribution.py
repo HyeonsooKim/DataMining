67afab583809bbd6edd3ecba23ff594fc485c8f5
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2020-11-16 08:34:25 +0900

    Update 16-pandas-sex-distribution.py

--- src/16-pandas-sex-distribution.py
+++ src/16-pandas-sex-distribution.py
@@ -1,19 +1,9 @@
-# https://www.kaggle.com/c/titanic/data
-# 데이터 분포 시각화
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('../data/train.csv')
 
 sex_count = data['Sex'].value_counts()
-df = pd.DataFrame([sex_count])
-df.index = ['Total']
-print(df)
-df.plot(kind='bar')
+print(sex_count)
+sex_count.plot(kind='bar')
 plt.show()
-
-#      male  female
-# Sex   577     314
-#
-# Process finished with exit code 0
