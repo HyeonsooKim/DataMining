a402cbc9e82fd0a94f4bc6e946fcdfb55d2bca38
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2020-11-16 05:41:48 +0900

    Create 17-pandas-sex-by-pclass-distribution.py

---
+++ src/17-pandas-sex-by-pclass-distribution.py
@@ -0,0 +1,16 @@
+# https://www.kaggle.com/c/titanic/data
+# 데이터 분포 시각화
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('../data/train.csv')
+
+pclass1_sex_count = data[data['Pclass'] == 1]['Sex'].value_counts()
+pclass2_sex_count = data[data['Pclass'] == 2]['Sex'].value_counts()
+pclass3_sex_count = data[data['Pclass'] == 3]['Sex'].value_counts()
+df = pd.DataFrame([pclass1_sex_count, pclass2_sex_count, pclass3_sex_count])
+df.index = ['Pclass1', 'Pclass2', 'Pclass3']
+print(df)
+df.plot(kind='bar')
+plt.show()
