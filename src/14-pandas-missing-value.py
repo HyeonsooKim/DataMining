ffe18317b536165d9a6631a5530023fc2b06a83b
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2020-11-16 08:34:19 +0900

    Update 14-pandas-missing-value.py

--- src/14-pandas-missing-value.py
+++ src/14-pandas-missing-value.py
@@ -1,24 +1,4 @@
-# https://www.kaggle.com/c/titanic/data
-# 결측값(null) 분포 확인
-
 import pandas as pd
 
 data = pd.read_csv('../data/train.csv')
 print(data.isnull().sum())
-
-# PassengerId      0
-# Survived         0
-# Pclass           0
-# Name             0
-# Sex              0
-# Age            177
-# SibSp            0
-# Parch            0
-# Ticket           0
-# Fare             0
-# Cabin          687
-# Embarked         2
-# dtype: int64
-#
-# Process finished with exit code 0
-
